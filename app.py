62bf93aa09d49de880d451237ea62306b640fcf7
Andrey Tvorozhkov <tvorog@tvorog.me>
2019-12-01 16:47:13 +0300

    Chnage padding

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from tkinter import font, Entry
 
 from config import *
 
+x_padding = 11
 
 class Printer(tkinter.Tk):
     def __init__(self):
@@ -15,7 +16,7 @@ class Printer(tkinter.Tk):
 
         self.field = Entry(self, width=33)
         self.field.insert(0, 'Фамилия Имя')
-        self.field.grid(row=1, columnspan=16, pady=(30, 10), padx=(7, 0))
+        self.field.grid(row=1, columnspan=16, pady=(30, 10), padx=(x_padding, 0))
 
         self.draw_keyboard()
         self.change()
@@ -51,7 +52,7 @@ class Printer(tkinter.Tk):
             button.grid(row=cur_row,
                         column=cur_column,
                         columnspan=columnspan,
-                        padx=0 if not cur_column == 0 else (7, 0)
+                        padx=0 if not cur_column == 0 else (x_padding, 0)
                         )
 
             cur_column += 1 if not big else 3
